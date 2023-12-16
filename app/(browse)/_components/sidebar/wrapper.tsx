3233aec62301be082ebf10e607c0d792a53d08a7
olegator888 <139753116+olegator888@users.noreply.github.com>
2023-12-16 18:52:16 +0300

    follow list

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -6,6 +6,7 @@ import { ReactNode } from "react";
 import { ToggleSkeleton } from "@/app/(browse)/_components/sidebar/toggle";
 import { RecommendedSkeleton } from "@/app/(browse)/_components/sidebar/recommended";
 import { useIsClient } from "usehooks-ts";
+import { FollowingSkeleton } from "@/app/(browse)/_components/sidebar/following";
 
 interface WrapperProps {
   children: ReactNode;
@@ -19,6 +20,7 @@ const Wrapper = ({ children }: WrapperProps) => {
     return (
       <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
         <ToggleSkeleton />
+        <FollowingSkeleton />
         <RecommendedSkeleton />
       </aside>
     );
