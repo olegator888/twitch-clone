fd97bfb8e12ba43ef632c2c20ffa283ee4a52c23
olegator888 <139753116+olegator888@users.noreply.github.com>
2023-12-17 17:12:50 +0300

    key settings

---
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+import { AlertTriangle } from "lucide-react";
+import {
+  Select,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+
+export const ConnectModal = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="primary">Generate connection</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Generate connection</DialogTitle>
+        </DialogHeader>
+        <Select>
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="Ingress Type" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="RTMP">RTMP</SelectItem>
+            <SelectItem value="WHIP">WHIP</SelectItem>
+          </SelectContent>
+        </Select>
+        <Alert>
+          <AlertTriangle className="h-4 w-4" />
+          <AlertTitle>Warning!</AlertTitle>
+          <AlertDescription>
+            This action will reset all active streams using the current
+            connection
+          </AlertDescription>
+        </Alert>
+        <div className="flex justify-between">
+          <DialogClose>
+            <Button variant="ghost">Cancel</Button>
+          </DialogClose>
+          <Button onClick={() => {}} variant="primary">
+            Generate
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
